7714d3c17040db924f9401a7c65853ffb69a3049
Maria Neise <maria.neise1@gmail.com>
2015-03-21 17:29:42 +1300

    Add support to process amd modules.

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [com.google.javascript/closure-compiler "v20150315"]]
+  :profiles {:uberjar {:aot :all}}
   :main hello-cc.core)
--- src/hello_cc/core.clj
+++ src/hello_cc/core.clj
@@ -2,37 +2,20 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [clojure.string :as s])
   (:import [java.util List]
+           [java.util.logging Level]
            [com.google.javascript.jscomp ProcessCommonJSModules
             CompilerOptions SourceFile Result JSError])
   (:gen-class main true))
 
-(def module-types ["commonjs", "amd"])
+(def module-types [:commonjs :amd])
 
-(defn report-failure [^Result result]
-  (let [errors (.errors result)
-        warnings (.warnings result)]
-    (doseq [next (seq errors)]
-      (println "ERROR:" (.toString ^JSError next)))
-    (doseq [next (seq warnings)]
-      (println "WARNING:" (.toString ^JSError next)))))
-
-(defn process-commonjs
-  [file]
-  (let [^List externs '()
-        ^List inputs (list (SourceFile/fromFile file))
-        ^CompilerOptions options (doto (CompilerOptions.)
-                                   (.setProcessCommonJSModules true))
-        compiler (com.google.javascript.jscomp.Compiler.)
-        ^Result result (.compile compiler externs inputs options)]
-    (if (.success result)
-      (println (.toSource compiler))
-      (report-failure result))))
 
 (def cli-options
-  ;; An option with a required argument
   [[nil "--js JS_FILE" "JavaScript File"
+    :parse-fn str
     :validate [#(not (s/blank? %)) "Please pass a valid JavaScript filename"]]
-   ["-m" "--module-type" "JavaScript module type"
+   ["-m" "--module-type TYPE" "JavaScript module type"
+    :parse-fn keyword
     :validate [(fn [v] (some #(= % v) module-types))
                (str "Please use one of the following module types: "
                     (s/join ", " module-types))]]
@@ -42,13 +25,48 @@
   (println msg)
   (System/exit status))
 
+(defn report-failure [^Result result]
+  (let [errors (.errors result)
+        warnings (.warnings result)]
+    (doseq [next (seq errors)]
+      (println "ERROR:" (.toString ^JSError next)))
+    (doseq [next (seq warnings)]
+      (println "WARNING:" (.toString ^JSError next)))))
+
+(defn ^com.google.javascript.jscomp.Compiler make-closure-compiler []
+  (let [compiler (com.google.javascript.jscomp.Compiler.)]
+    (com.google.javascript.jscomp.Compiler/setLoggingLevel Level/WARNING)
+    compiler))
+
+(defn set-options [opts ^CompilerOptions compiler-options]
+  (case (:type opts)
+    :commonjs (.setProcessCommonJSModules compiler-options true)
+    :amd (doto compiler-options
+           (.setProcessCommonJSModules true)
+           (.setTransformAMDToCJSModules true)))
+  compiler-options)
+
+(defn process-js-module
+  [file type]
+  (let [^List externs '()
+        ^List inputs (list (SourceFile/fromFile file))
+        ^CompilerOptions options (set-options {:type type} (CompilerOptions.))
+        compiler (make-closure-compiler)
+        ^Result result (.compile compiler externs inputs options)]
+    (if (.success result)
+      (println (.toSource compiler))
+      (report-failure result))))
+
 (defn -main
   [& args]
-  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
-    ;; Handle help and error conditions
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)
+        errors (if-not (:js options)
+                 (conj errors "Please pass a JavaScript filename")
+                 errors)
+        errors (if-not (:module-type options)
+                 (conj errors "Please specify a module type")
+                 errors)]
     (cond
       (:help options) (exit 0 summary)
-      errors (exit 1 (s/join errors)))
-    (if (:js options)
-      (process-commonjs (:js options))
-      (exit 1 "Please pass a JavaScript filename"))))
+      errors (exit 1 (s/join "\n" errors)))
+    (process-js-module (:js options) (:module-type options))))

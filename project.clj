5767cd22945ebf4fa46a702cedee6ddbd496918e
Maria Neise <maria.neise1@gmail.com>
2015-03-22 00:37:43 +1300

    Update project description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hello-cc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "hello-cc: Simple command line app for processing JS modules."
+  :url "https://github.com/MNeise/hello-cc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
